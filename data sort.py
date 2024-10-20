603e10e4b602b928ba32460a632978f84d678f40
Korshunova Anastasiia <74792250+AkyLkaNastya@users.noreply.github.com>
2024-10-20 22:10:43 +0300

    Оптимизировано
    Убрала лишние переменные и перевалочные пункты. Подробно объяснила процесс

--- data sort.py
+++ data sort.py
@@ -2,33 +2,28 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('stock_data_2018.csv')
-tickers = data['Ticker'].unique() # Create a list of unique tickers names
+tickers = data['Ticker'].unique()
 
-# Create a DataFrame to store the closing prices for each ticker
-dates = pd.DataFrame()
-dates['Date'] = sorted(list(data['Date'].unique()))
-dates = dates.set_index('Date')
+# Создаем пустой DataFrame, в котором даты будут индексами
+new_data = pd.DataFrame()
+new_data['Date'] = sorted(list(data['Date'].unique()))
+new_data = new_data.set_index('Date')
 
-dates_log = dates
+'''____________________________________________________________________________________________________________
 
-'''__________________________________________________________________________________
-
-Проходимся по каждому тикеру и 
-__________________________________________________________________________________'''
+Проходимся по каждому тикеру и создаем табличку с данными закрытия торгов для этого тикера.
+Из столбца 'Date' снова делаем индексы.
+Считаем логарифмическую доходность по каждому дню, используя shift.
+Теперь, когда мы добавляем получившиеся значения в new_data, данные раскидываются по соответствующим датам.
+Если в какой-то день торгов не было, то ячейка остается незаполненной.
+____________________________________________________________________________________________________________'''
 
 for ticker in tickers[:1]:
     current_ticker = pd.DataFrame()
-    current_ticker = current_ticker.assign(data[data['Ticker'] == ticker]['Date'], data[data['Ticker'] == ticker]['Close'])
-    # current_ticker['Date'] = data[data['Ticker'] == ticker]['Date']
-    # current_ticker['Close'] = data[data['Ticker'] == ticker]['Close']
-    print(current_ticker)
-    # current_ticker = current_ticker.set_index('Date')
+    current_ticker['Date'] = data[data['Ticker'] == ticker]['Date']
+    current_ticker['Close'] = data[data['Ticker'] == ticker]['Close']
+    current_ticker = current_ticker.set_index('Date')
+    
+    new_data[ticker] = np.log(current_ticker['Close'] / current_ticker['Close'].shift(1))
 
-    # dates[ticker] = current_ticker['Close']
-
-    # test = pd.DataFrame()
-    # test['log_return'] = np.log(dates[ticker] / dates[ticker].shift(1))
-    # dates_log[ticker] = test['log_return']
-
-
-# data.to_csv('test.csv')
+new_data.to_csv('test.csv')
