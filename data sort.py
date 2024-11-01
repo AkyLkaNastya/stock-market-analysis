72a38d229f85cb2b9779bd01167df322f66757f0
Korshunova Anastasiia <korshunova.acp@bk.ru>
2024-11-01 13:18:18 +0300

    Переименовала файл с кодом, сделала 1.5

--- data sort.py
+++ data sort.py
@@ -1,44 +1,31 @@
 import pandas as pd
 import numpy as np
 
-# data = pd.read_csv('stock_data_2018.csv')
-# tickers = data['Ticker'].unique()
+data = pd.read_csv('stock_data_2018.csv')
+tickers = data['Ticker'].unique()
 
-data1 = pd.read_csv('data_log_return.csv')
-tickers1 = list(data1.columns)
+# Создаем пустой DataFrame, в котором даты будут индексами
+new_data = pd.DataFrame()
+new_data['Date'] = sorted(list(data['Date'].unique()))
+new_data = new_data.set_index('Date')
 
-data2 = pd.read_csv('dates_log.csv')
-tickers2 = list(data2.columns)
+'''____________________________________________________________________________________________________________
 
-if tickers1 == tickers2:
-    print('Тикеры совпадают.')
-else:
-    print('Тикеры не совпадают.')
+Проходимся по каждому тикеру и создаем табличку с данными закрытия каждого дня торгов для этого актива.
+Из столбца 'Date' снова делаем индексы.
+Считаем логарифмическую доходность по каждому дню, используя shift.
+Теперь, когда мы добавляем получившиеся значения в new_data, данные раскидываются по соответствующим датам.
+Если в какой-то день торгов не было, то ячейка остается незаполненной (NaN).
+____________________________________________________________________________________________________________'''
 
-# # Создаем пустой DataFrame, в котором даты будут индексами
-# new_data = pd.DataFrame()
-# new_data['Date'] = sorted(list(data['Date'].unique()))
-# new_data = new_data.set_index('Date')
-
-# '''____________________________________________________________________________________________________________
-
-# Проходимся по каждому тикеру и создаем табличку с данными закрытия торгов для этого тикера.
-# Из столбца 'Date' снова делаем индексы.
-# Считаем логарифмическую доходность по каждому дню, используя shift.
-# Теперь, когда мы добавляем получившиеся значения в new_data, данные раскидываются по соответствующим датам.
-# Если в какой-то день торгов не было, то ячейка остается незаполненной.
-# ____________________________________________________________________________________________________________'''
-
-# for ticker in tickers:
-#     current_ticker = pd.DataFrame()
-#     current_ticker['Date'] = data[data['Ticker'] == ticker]['Date']
-#     if current_ticker.shape[0] < 200:
-#         continue
-#     current_ticker['Close'] = data[data['Ticker'] == ticker]['Close']
-#     current_ticker = current_ticker.set_index('Date')
+for ticker in tickers:
+    current_ticker = pd.DataFrame()
+    current_ticker['Date'] = data[data['Ticker'] == ticker]['Date']
+    if current_ticker.shape[0] < 200: # Если у актива меньше 200 дней торгов, то не добавляем его в табличку
+        continue
+    current_ticker['Close'] = data[data['Ticker'] == ticker]['Close']
+    current_ticker = current_ticker.set_index('Date')
     
-#     new_data[ticker] = np.log(current_ticker['Close'] / current_ticker['Close'].shift(1))
-
-# print(new_data)
-
-#new_data.to_csv('data_log_return.csv')
+    new_data[ticker] = np.log(current_ticker['Close'] / current_ticker['Close'].shift(1))
+    
+new_data.to_csv('data_log_return.csv')
--- Часть 1/code_part.py
+++ Часть 1/code_part.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import stats
 from scipy.optimize import minimize
 
-#pd.set_option('display.max_rows',None)
+pd.set_option('display.max_rows',None)
 
 data = pd.read_csv("data_log_return.csv")
 tickers = list(data.columns)
@@ -78,44 +78,104 @@ pareto_optimal = find_E_n_sigma(data, pareto_optimal_assets)
 '''      
 ''' =========================================================================================================================== '''
 
-def make_returns(portfolio):
-    portfolio_returns = pd.DataFrame()
-    ticker_returns = pd.DataFrame()
+# def make_returns(portfolio):
+#     ticker_returns = pd.DataFrame()
 
-    for ticker in portfolio['Ticker']:
-        ticker_returns['log_return'] = pd.DataFrame(data[ticker]).reset_index(drop=True)
-        portfolio_returns[ticker] = ticker_returns['log_return']
-    portfolio_returns['Portfolio Returns'] = portfolio_returns.sum(axis=1)
+#     for ticker in portfolio:
+#         ticker_returns['log_return'] = pd.DataFrame(data[ticker]).reset_index(drop=True)
     
-    return portfolio_returns['Portfolio Returns']
+#     return ticker_returns['log_return']
 
-def historicalVaR(returns, alpha):
+# def historicalVaR(returns, alpha):
 
-    if isinstance(returns, pd.Series):
-        return np.percentile(returns, alpha)
+#     if isinstance(returns, pd.Series):
+#         return np.percentile(returns, alpha)
 
-    elif isinstance(returns, pd.DataFrame):
-        return returns.aggregate(historicalVaR, alpha=alpha)
+#     elif isinstance(returns, pd.DataFrame):
+#         return returns.aggregate(historicalVaR, alpha=alpha)
 
+#     else:
+#         raise TypeError("Expected returns to be dataframe or series")
+
+# def historicalCVaR(returns, alpha):
+
+#     if isinstance(returns, pd.Series):
+#         belowVaR = returns <= historicalVaR(returns, alpha=alpha)
+#         return returns[belowVaR].mean()
+
+#     elif isinstance(returns, pd.DataFrame):
+#         return returns.aggregate(historicalCVaR, alpha=alpha)
+
+#     else:
+#         raise TypeError("Expected returns to be dataframe or series")
+
+# for ticker in pareto_optimal_assets:
+#     returns = pd.DataFrame(data[ticker])
+#     VaR = -historicalVaR(returns, 95)
+#     CVaR = -historicalCVaR(returns, 95)
+#     # print(f'{ticker}: VaR:  {VaR}, CVaR: {CVaR}')
+#     print(VaR)
+
+''' =========  № 4  =========================================================================================================== '''
+'''      Распределение доходностей конкретного актива
+###
+###      Выбор нескольких интересных (значимых) активов рынка.
+###      Можно ли считать наблюдаемые доходности конкретного актива повторной выборкой из некоторого распределения (белый шум)?
+###      Поиск научных подходов к ответу на этот вопрос.
+'''      
+''' =========================================================================================================================== '''
+
+
+
+''' =========  № 5  =========================================================================================================== '''
+'''      Нормальность распределений доходностей
+###
+###      В предположении, что наблюдаемые доходности выбранных активов являются повторной выборкой из некоторого распределения исследовать (выборочно) распределения доходностей выбранных активов.
+###      Можно ли считать, что распределения доходностей подчиняются нормальному закону распределения?
+###      Если ответ отрицательный, какие другие законы распределения доходностей соответствуют данным наблюдений?
+'''      
+''' =========================================================================================================================== '''
+
+import seaborn as sns
+from scipy import stats
+
+selected_tickers = []
+
+for ticker in tickers:
+    returns = data[ticker].dropna() # Удаляем NaN, если они есть
+    lst = data[ticker].tolist()
+    if returns.shape[0] == 245 and lst.count(0) <= 1 and risk_and_return.loc[risk_and_return['Ticker'] == ticker]['E'].values[0] > 0:
+        selected_tickers.append(ticker)
+    if len(selected_tickers) >= 5:
+        break
+
+for ticker in selected_tickers:
+    returns = data[ticker].dropna() # Удаляем NaN, если они есть
+
+    plt.figure(figsize=(7, 3))
+    plt.subplot(1, 2, 1)
+    sns.histplot(returns, bins=50, kde=True)
+    plt.title(f'Гистограмма: {ticker}')
+    plt.xlabel('Логарифмическая доходность')
+    plt.ylabel('Частота')
+
+    # QQ plot
+    plt.subplot(1, 2, 2)
+    stats.probplot(returns, dist="norm", plot=plt)
+    plt.title(f'QQ Plot')
+    plt.xlabel('')
+    plt.ylabel('')
+
+    # Показ графиков
+    plt.tight_layout()
+    plt.show()
+
+    print('='*10, ticker, '='*40, '\n')
+
+    # Тест Шапиро-Уилка
+    stat, p_value = stats.shapiro(returns)
+    print(f'Статистика = {stat}, p-значение = {p_value}')
+    if p_value > 0.05:
+        print(f'Распределение логарифмических доходностей можно считать нормальным (p > 0.05) \n')
     else:
-        raise TypeError("Expected returns to be dataframe or series")
-
-def historicalCVaR(returns, alpha):
-
-    if isinstance(returns, pd.Series):
-        belowVaR = returns <= historicalVaR(returns, alpha=alpha)
-        return returns[belowVaR].mean()
-
-    elif isinstance(returns, pd.DataFrame):
-        return returns.aggregate(historicalCVaR, alpha=alpha)
-
-    else:
-        raise TypeError("Expected returns to be dataframe or series")
-
-for ticker in pareto_optimal_assets:
-    
-    returns = pd.DataFrame(data[ticker])
-
-    VaR = -historicalVaR(returns, 95)
-    CVaR = -historicalCVaR(returns, 95)
-    print(f'{ticker}: VaR:  {VaR}, CVaR: {CVaR}')
+        print(f'Распределение логарифмических доходностей не является нормальным (p <= 0.05) \n')
